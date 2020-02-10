20a316480d210fd545f749b80b76a7afe4f8b732
tony-fernandez <mr_fernandez@yahoo.com>
2020-02-10 05:49:01 +0100

    Demo soap service

---
+++ src/main/java/com/example/demo/service/CountryEndpoint.java
@@ -0,0 +1,33 @@
+package com.example.demo.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.ws.server.endpoint.annotation.Endpoint;
+import org.springframework.ws.server.endpoint.annotation.PayloadRoot;
+import org.springframework.ws.server.endpoint.annotation.RequestPayload;
+import org.springframework.ws.server.endpoint.annotation.ResponsePayload;
+
+import com.example.demo.repository.CountryRepository;
+import com.example.demo.ws.GetCountryRequest;
+import com.example.demo.ws.GetCountryResponse;
+
+
+@Endpoint
+public class CountryEndpoint {
+	private static final String NAMESPACE_URI = "http://spring.io/guides/gs-producing-web-service";
+
+	private CountryRepository countryRepository;
+
+	@Autowired
+	public CountryEndpoint(CountryRepository countryRepository) {
+		this.countryRepository = countryRepository;
+	}
+
+	@PayloadRoot(namespace = NAMESPACE_URI, localPart = "getCountryRequest")
+	@ResponsePayload
+	public GetCountryResponse getCountry(@RequestPayload GetCountryRequest request) {
+		GetCountryResponse response = new GetCountryResponse();
+		response.setCountry(countryRepository.findCountry(request.getName()));
+
+		return response;
+	}
+}
